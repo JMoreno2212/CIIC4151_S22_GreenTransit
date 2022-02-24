373349757efe087c918a5cdc4798013e91454a5d
Jesús A. Moreno Acevedo <jesus.moreno@upr.edu>
2022-02-24 16:11:02 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def hello_world():
+    return "Welcome to the Green Transit App Homepage"
+
+
+# --------------------------------------------------------------------------------------
+# Main
+# --------------------------------------------------------------------------------------
+if __name__ == 'main':
+    app.run()
