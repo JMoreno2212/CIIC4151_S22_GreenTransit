44a0ffe2e39816eac7fdf93e0e6529e4059bfebd
Jesus Moreno <moreno.jesus2212@gmail.com>
2022-03-03 15:47:28 -0400

    Created Endpoint to Get All Users

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+from backend.controller.user import BaseUser
+
 app = Flask(__name__)
 CORS(app)
 
@@ -8,6 +10,17 @@ CORS(app)
 @app.route('/')
 def hello_world():
     return "Welcome to the Green Transit App Homepage"
+
+
+# --------------------------------------------------------------------------------------
+# User
+# --------------------------------------------------------------------------------------
+@app.route('/User/users', methods=['GET'])
+def handleUsers():
+    if request.method == 'GET':
+        return BaseUser().getAllUsers()
+    else:
+        return jsonify("Method Not Allowed"), 405
 
 
 # --------------------------------------------------------------------------------------
