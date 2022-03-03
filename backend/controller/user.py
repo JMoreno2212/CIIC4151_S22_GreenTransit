76212b4840c86f90551cd2e913365216dc44c90a
Jesus Moreno <moreno.jesus2212@gmail.com>
2022-03-03 15:47:13 -0400

    Created Endpoint to Get All Users

---
+++ backend/controller/user.py
@@ -0,0 +1,24 @@
+from flask import jsonify
+
+from backend.model.user import UserDAO
+
+
+def build_user_map_dict(row):
+    result = {'user_id': row[0], 'user_first_name': row[1], 'user_last_name': row[2], 'user_birth_date': row[3],
+              'user_phone': row[4], 'user_email': row[5], 'user_password': row[6], 'license_id': row[7],
+              'user_Active': row[8]}
+    return result
+
+
+class BaseUser:
+    def getAllUsers(self):
+        user_dao = UserDAO()
+        users_list = user_dao.getAllUsers()
+        if not users_list:  # User List is empty
+            return jsonify("No Users Found"), 404
+        else:
+            result_list = []
+            for row in users_list:
+                obj = build_user_map_dict(row)
+                result_list.append(obj)
+            return jsonify(result_list), 200
---
+++ backend/model/user.py
@@ -0,0 +1,26 @@
+import psycopg2
+
+from backend.config.dbconfig import db_root_config
+
+
+class UserDAO:
+    def __init__(self):
+        connection_url = "dbname=%s user=%s password=%s port=%s host=%s" % (db_root_config['dbname'],
+                                                                            db_root_config['user'],
+                                                                            db_root_config['password'],
+                                                                            db_root_config['dbport'],
+                                                                            db_root_config['host'])
+        self.conn = psycopg2.connect(connection_url)
+
+    # ----------------------------------------------------------------------------------------------------------------
+    #                                                      Read                                                      #
+    # ----------------------------------------------------------------------------------------------------------------
+    def getAllUsers(self):
+        cursor = self.conn.cursor()
+        query = 'select * from "User";'
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        cursor.close()
+        return result
