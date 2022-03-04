227b1aa3e8d3a3c454e8da12db4239488d45e3f0
Jesus Moreno <moreno.jesus2212@gmail.com>
2022-03-04 12:58:57 -0400

    Removed config folder and added specific environment folder

--- backend/model/dispensary.py
+++ backend/model/dispensary.py
@@ -1,16 +1,11 @@
-import psycopg2
+import os
 
-from backend.config.dbconfig import db_root_config
+import psycopg2
 
 
 class DispensaryDAO:
     def __init__(self):
-        connection_url = "dbname=%s user=%s password=%s port=%s host=%s" % (db_root_config['dbname'],
-                                                                            db_root_config['user'],
-                                                                            db_root_config['password'],
-                                                                            db_root_config['dbport'],
-                                                                            db_root_config['host'])
-        self.conn = psycopg2.connect(connection_url)
+        self.conn = psycopg2.connect(os.getenv('DATABASE_URL'))
 
     # ----------------------------------------------------------------------------------------------------------------
     #                                                      Read                                                      #
@@ -31,4 +26,3 @@ class DispensaryDAO:
         cursor.execute(query, (dispensary_id,))
         result = cursor.fetchone()
         return result
-
--- backend/model/item.py
+++ backend/model/item.py
@@ -1,16 +1,11 @@
-import psycopg2
+import os
 
-from backend.config.dbconfig import db_root_config
+import psycopg2
 
 
 class ItemDAO:
     def __init__(self):
-        connection_url = "dbname=%s user=%s password=%s port=%s host=%s" % (db_root_config['dbname'],
-                                                                            db_root_config['user'],
-                                                                            db_root_config['password'],
-                                                                            db_root_config['dbport'],
-                                                                            db_root_config['host'])
-        self.conn = psycopg2.connect(connection_url)
+        self.conn = psycopg2.connect(os.getenv('DATABASE_URL'))
 
     # ----------------------------------------------------------------------------------------------------------------
     #                                                      Read                                                      #
